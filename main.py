33c5148251aa30629953a7e0baf6ea8a0f6f5b98
ClaireW <claira.m@live.fr>
2020-12-04 10:58:20 +0100

    ajout de commentaires

--- main.py
+++ main.py
@@ -7,12 +7,13 @@ import os
 
 args = sys.argv
 
+# On boucle dans nos arguments, si l'un d'eux est "-i", notre dossier d'entrée prend la valeur écrite juste après
 for i in range(0, len(args)):
     arg = args[i]
     if arg == '-i':
         dossierE = f'{args[i+1]}'
 
-
+# On boucle dans nos arguments, si l'un d'eux est "-o", notre dossier de sortie prend la valeur écrite juste après
 for i in range(0, len(args)):
         arg = args[i]
         if arg == '-o':
@@ -20,17 +21,19 @@ for i in range(0, len(args)):
             if not os.path.exists(args[i+1]):
                 os.mkdir(args[i + 1])
 
-
+# On boucle dans nos arguments, si l'un d'eux est "--blur", alors on lance la fonction blur_pic
 for i in range(0, len(args)):
     arg = args[i]
     if arg == '--blur':
         blur_pic(dossierE, dossierS, 13)
 
+# On boucle dans nos arguments, si l'un d'eux est "--dilate", alors on lance la fonction dilatation_pic
 for i in range(0, len(args)):
     arg = args[i]
     if arg == '--dilate':
         dilatation_pic(dossierS,dossierS)
 
+# On boucle dans nos arguments, si l'un d'eux est "--gray", alors on lance la fonction gray_pic
 for i in range(0, len(args)):
     arg = args[i]
     if arg == '--gray':
@@ -42,6 +45,7 @@ for i in range(0, len(args)):
 
 first_arg = args [1]
 
+#Si mon premier argument est '-h' alors je fais tous ces prints
 if first_arg == '-h':
     print(args)
     print('usage: imagefilter')
@@ -49,6 +53,8 @@ if first_arg == '-h':
     print("-i, --input-dir <imgs>")
     print("-o, --output-dir <>")
     print("--blur --dilate -- gray")
+
+#Sinon si mon premier argument est '---help' alors je fais tous ces prints
 elif first_arg == '---help':
     print('write "-i" for choose an input dir')
     print('write "-o" for choose an output dir')
